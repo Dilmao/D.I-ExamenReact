de36467d0f252c9906cb98a243167e852a3fb9b2
Alumno <200066@iessanalberto.com>
2023-11-28 13:59:05 +0100

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-//PetStore
-
+import { useEffect, useState } from 'react';
 import PetStore from './PetStore';
+//Failed to fetch
+
+const DEFAULT_URL = "http://petstore-demo-endpoint.execute-api.com/petstore/pets?type=cat&page=1";
+
+// Peta
+async function fetchPosts() {
+    const response = await fetch(DEFAULT_URL);
+    const blogPosts = await response.json();
+    return blogPosts;
+}
 
 function App() {
-  return (
-    <div>
-      <PetStore />
-    </div>
-  );
+    const [loadedPost, setLoadedPosts] = useState([])
+
+    useEffect(function () {
+        fetchPosts().then((fetchedPosts) => setLoadedPosts(fetchedPosts));
+    }, []);
+
+    return (
+        <div>
+            <PetStore Masctas={loadedPost}/>
+        </div>
+    );
 }
 
 export default App;
