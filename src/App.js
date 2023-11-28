87ec114e660bb58b5e9641ba5ccc9e6cf9270690
Diego <130553232+Dilmao@users.noreply.github.com>
2023-11-28 14:06:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,39 +2,43 @@ import { useState } from 'react';
 import './App.css';
 import EnrolList from './EnrolList';
 import Formulario from './Form/Formulario';
-//Faltan las props
 
+//Se crea un usuario inicial para comprobar si al menos se muestra
 const initialUser = [ 
-  { id: 1, fname: "Diego",  lname: "Lacasa", program: "UG", email: "Diego@gmail"}
+    { id: 1, fname: "Diego",  lname: "Lacasa", program: "UG", email: "Diego@gmail"}
 ]
 
 function App() {
-  const [users, setUsers] = useState(initialUser)
-  const [value, setValue] = useState("")
+    //Declaracion de variables
+    const [users, setUsers] = useState(initialUser)
+    const [value, setValue] = useState("")
 
-  function handleAddUser(event) {
-    setValue(event.target.value)
-  }
-
-  let contador = users.length
-
-  function AddList() {
-    if (value.trim() === "") {
-      return;
+    //Funciones que no funcionan
+    function handleAddUser(event) {
+        setValue(event.target.value)
     }
 
-    const newUser = { id: contador, name:value }
-    setUsers([...users, newUser])
-    setValue("")
-  }
+    let contador = users.length
 
-  return (
-    <div className='App'>
-      <Formulario ChangeInput={handleAddUser} Value={value} ClickButton={AddList}/>
-      <hr/>
-      <EnrolList Users={users}/>
-    </div>
-  );
+    function AddList() {
+        if (value.trim() === "") {
+          return;
+        }
+
+        const newUser = { id: contador, name:value }
+        setUsers([...users, newUser])
+        setValue("")
+    }
+
+    //Faltan los props
+    return (
+        <div className='App'>
+            <Formulario ChangeInput={handleAddUser} Value={value} ClickButton={AddList}/>
+            <hr/>
+            <EnrolList Users={users}/>
+        </div>
+    );
 }
 
 export default App;
+
